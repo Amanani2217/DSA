f5e9cff7e54c24b575b53aebe0e483c276550d32
Aman Bansal <139121157+Amanani2217@users.noreply.github.com>
2024-09-29 20:34:02 +0530

    Time: 592 ms (61.83%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,3 @@
-# Write your MySQL query statement bel
+with temp as (select customer_id,min(order_date)as order_date from delivery group by customer_id) 
 
-with temp as(
-    select customer_id,min(order_date) as fi from delivery group by customer_id
-)
-
-select round((count(*)/(select count(distinct customer_id) from delivery ))*100 ,2)as immediate_percentage from delivery  d join temp t on d.customer_id=t.customer_id where t.fi =d.customer_pref_delivery_date;
+select round((count(*)/(select count(distinct customer_id) from delivery))*100,2) as immediate_percentage from delivery d  join temp t on d.customer_id = t.customer_id where d.customer_pref_delivery_date = t.order_date
