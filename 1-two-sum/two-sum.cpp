362a8b79dcf5fa1c90925debd6c17c834cdf01cf
Aman Bansal <139121157+Amanani2217@users.noreply.github.com>
2024-12-16 22:42:52 +0530

    Time: 3 ms (77.98%) | Memory: 15.7 MB (10.31%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         vector<int>v;
-         //this is the brute force
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     for(int j=i+1;j<nums.size();j++)
-        //     {
-        //         if(nums[i]+nums[j]==target)
-        //            {v.push_back(i);
-        //             v.push_back(j);
-        //             break;
-        //            }
-        //     }
-        // }
-
-        //better sol using hash map
-        map<int,int>m;
-        for(int i = 0 ;i<nums.size();i++){
-            int x = target - nums[i];
-            if(m[x]){
+       map<int,int>m;
+       vector<int>v;
+       for(int i =0;i<nums.size();i++){
+         int x = target -nums[i];
+         if(m[x]){
             v.push_back(m[x]-1);
-                v.push_back(i);
-                break;
-            }
-            m[nums[i]]=i+1;
-        }
-       
-    return v;
+            v.push_back(i);
+            break;
+         }
+         m[nums[i]] = i+1;
+       }
+       return v;
     }
 };
