f54f00dc437c50f2458f20e34889ae408bcad386
Aman Bansal <139121157+Amanani2217@users.noreply.github.com>
2024-12-31 19:32:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.cpp
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -1,78 +1,68 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    int select(int arr[], int i)
-    {
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
         
-    }
-     void swap(int arr[],int x, int y ){
-         int temp = arr[x];
-         arr[x]=arr[y];
-         arr[y]=temp;
-         
-     }
-    void selectionSort(int arr[], int n)
-    {
-       //code here
-       int min = 0 ;
-       for(int i =0 ;i<n;i++){
-           int j = i;
-           min = i;
-           while(j<n){
-               if(arr[min]>arr[j]){
-                   min = j;
-               
-               }
-                   j++;
-           }
-           swap(arr,min,i);
-           
-       }
+        for(int i=0;i<arr.size();i++){
+            int min=i;
+            for(int j =i+1 ;j<arr.size();j++){
+                if(arr[min]>arr[j]){
+                    
+                    min=j;
+                }
+            }
+            swap(arr[min],arr[i]);
+        }
     }
 };
 
 //{ Driver Code Starts.
- 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
- 
-// Driver program to test above functions
-int main()
-{
-    int arr[1000],n,T,i;
-  
-    scanf("%d",&T);
-    
-    while(T--){
-        
-    scanf("%d",&n);
-    
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      
-    Solution ob;  
-    ob.selectionSort(arr, n);
-    printArray(arr, n);
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution obj;
+        obj.selectionSort(a);
+
+        Array::print(a);
+        cout << "~" << endl;
     }
-    return 0;
 }
 
 // } Driver Code Ends
