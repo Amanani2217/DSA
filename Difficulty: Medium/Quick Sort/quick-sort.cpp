bcb0005d67ca01f9e0f6d20d0ef233d7bb7d3ba9
Aman Bansal <139121157+Amanani2217@users.noreply.github.com>
2025-01-02 16:26:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -1,69 +1,69 @@
 //{ Driver Code Starts
-#include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
 
 /* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        printf("%d ", num);
     printf("\n");
 }
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to sort an array using quick sort algorithm.
-    void quickSort(int arr[], int low, int high)
-    {
+class Solution {
+  public:
+    // Function to sort an array using quick sort algorithm.
+    void quickSort(vector<int>& arr, int low, int high) {
         // code here
-        if(low>=high)return;
-        int in = partition(arr,low,high);
-        quickSort(arr,low,in-1);
-        quickSort(arr,in+1,high);
+        if(low<high){
+        int part = partition(arr,low,high);
+        quickSort(arr,low,part-1);
+        quickSort(arr,part+1,high);
+        }
     }
-    
-    public:
-    void swap(int arr[],int x ,int y ){
-        int t = arr[x];
-        arr[x]=arr[y];
-        arr[y]=t;
-    }
-    int partition (int arr[], int l, int r)
-    {
-       // Your code here
-       int pivot = arr[l];
-       int i = l;int j = r;
-       while(i<j){
-          while(arr[i]<=pivot)
-              i++;
-          while(arr[j]>pivot)
-              j--;
-           if(i<j)
-             swap(arr,i,j);
-       }
-       swap(arr,j,l);
-       return j;
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+        
+        int pivot = high;
+        int i  = low;
+        for(int j =low;j<high;j++){
+            if(arr[j]<arr[pivot]){
+                swap(arr[i],arr[j]);
+                i++;
+            }
+        }
+        swap(arr[i],arr[pivot]);
+        return i;
     }
 };
 
-
 //{ Driver Code Starts.
-int main()
-{
-    int arr[1000],n,T,i;
-    scanf("%d",&T);
-    while(T--){
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      Solution ob;
-    ob.quickSort(arr, 0, n-1);
-    printArray(arr, n);
+
+int main() {
+
+    int T;
+    scanf("%d", &T);
+    getchar(); // to consume newline after T
+
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.quickSort(arr, 0, arr.size() - 1);
+        printArray(arr);
     }
+
     return 0;
 }
+
 // } Driver Code Ends
