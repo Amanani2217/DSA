f3527f6ca228d7d068b4109bdd519fb58652d042
Aman Bansal <139121157+Amanani2217@users.noreply.github.com>
2025-01-24 23:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (49.06%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k =0 ;
-        for(int i=0 ;i<nums.size();i++){
-            if(nums[i]==0)
-              {
-                k++;
-                nums.erase(nums.begin()+i);
-                i--;
-              }
-        }
-        
-        int i = nums.size();
-        while(k--){
-           nums.push_back(0);
-           i++;
+        int j=0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
     }
 };
